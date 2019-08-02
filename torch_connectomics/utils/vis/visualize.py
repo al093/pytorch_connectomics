5a476e9bca65714de47473265a01071f6741ae6c
Alok Verma <verma.alok001@gmail.com>
2019-08-02 17:14:04 -0400

    Visualize 1 channel mask images in Tensorboard in better manner

--- torch_connectomics/utils/vis/visualize.py
+++ torch_connectomics/utils/vis/visualize.py
@@ -1,7 +1,7 @@
 import torch
 import torchvision.utils as vutils
 
-N = 9 # default maximum number of sections to show
+N = 15 # default maximum number of sections to show
 min_batch = 3
 def prepare_data(volume, label, output):
 
@@ -24,7 +24,7 @@ def prepare_data(volume, label, output):
         else:
             return volume, label, output
 
-def visualize(volume, label, output, iteration, writer):
+def visualize(volume, label, output, iteration, writer, mode='Train'):
     volume, label, output = prepare_data(volume, label, output)
 
     sz = volume.size() # z,c,y,x
@@ -32,13 +32,21 @@ def visualize(volume, label, output, iteration, writer):
     output_visual = output.detach().cpu().expand(sz[0],3,sz[2],sz[3])
     label_visual = label.detach().cpu().expand(sz[0],3,sz[2],sz[3])
 
-    volume_show = vutils.make_grid(volume_visual, nrow=N, normalize=True, scale_each=True)
-    output_show = vutils.make_grid(output_visual, nrow=N, normalize=True, scale_each=True)
-    label_show = vutils.make_grid(label_visual, nrow=N, normalize=True, scale_each=True)
+    canvas = []
+    for idx in range(volume_visual.shape[0]):
+        canvas.append(volume_visual[idx])
+        canvas.append(label_visual[idx])
+        canvas.append(output_visual[idx])
 
-    writer.add_image('Input', volume_show, iteration)
-    writer.add_image('Label', label_show, iteration)
-    writer.add_image('Output', output_show, iteration)
+    canvas_show = vutils.make_grid(canvas, nrow=3, normalize=False, scale_each=True)
+    writer.add_image(mode + ' Mask', canvas_show, iteration)
+
+    # volume_show = vutils.make_grid(volume_visual, nrow=N, normalize=False, scale_each=True)
+    # output_show = vutils.make_grid(output_visual, nrow=N, normalize=False, scale_each=True)
+    # label_show = vutils.make_grid(label_visual, nrow=N, normalize=False, scale_each=True)
+    # writer.add_image('Input', volume_show, iteration)
+    # writer.add_image('Label', label_show, iteration)
+    # writer.add_image('Output', output_show, iteration)
 
 def visualize_aff(volume, label, output, iteration, writer, mode='Train'):
     volume, label, output = prepare_data(volume, label, output)
