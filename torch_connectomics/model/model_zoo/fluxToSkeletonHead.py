Divergence calculation was wrong, DDP loading checkpoint was wrong

torch_connectomics/model/model_zoo/fluxToSkeletonHead.py
@@ -49,9 +49,9 @@ class FluxToSkeletonHead(nn.Module):
 
     def _3d_divergence(self, input):
         input_pad = self.pad_fn(input)
-        dx = input_pad[:, 0, 1:-1, 1:-1, 0:-2] - input[:, 0]
+        dx = input_pad[:, 2, 1:-1, 1:-1, 0:-2] - input[:, 2]
         dy = input_pad[:, 1, 1:-1, 0:-2, 1:-1] - input[:, 1]
-        dz = input_pad[:, 2, 0:-2, 1:-1, 1:-1] - input[:, 2]
+        dz = input_pad[:, 0, 0:-2, 1:-1, 1:-1] - input[:, 0]
         return (dz + dy + dx).unsqueeze(1)
 
     def _get_padding_kernel_size(self, half_kernel_size, dilation=(1, 1, 1)):

torch_connectomics/utils/net/misc.py
@@ -113,13 +113,13 @@ def setup_model(args, device, model_io_size, exact=True, size_match=True, non_li
         print('Loading pretrained model:')
         print(args.pre_model)
         if exact:
-            checkpoint = torch.load(args.pre_model)
+            checkpoint = torch.load(args.pre_model, map_location=device)
             if checkpoint.get('model_state_dict', None):
                 model.load_state_dict(checkpoint['model_state_dict'])
             else:
                 model.load_state_dict(checkpoint)
         else:
-            pretrained_dict = torch.load(args.pre_model)
+            pretrained_dict = torch.load(args.pre_model, map_location=device)
             model_dict = model.state_dict()
             # 1. filter out unnecessary keys
             pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
@@ -134,11 +134,11 @@ def setup_model(args, device, model_io_size, exact=True, size_match=True, non_li
             model.load_state_dict(model_dict)
     return model
 
-def restore_state(optimizer, scheduler, args):
+def restore_state(optimizer, scheduler, args, device):
     if bool(args.load_model) and args.warm_start:
         if args.local_rank in [None, 0]:
             print('Trying to load optimizer and scheduler state from checkpoint.')
-        checkpoint = torch.load(args.pre_model)
+        checkpoint = torch.load(args.pre_model, map_location=device)
         if checkpoint.get('scheduler_state_dict', None):
             scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
             if args.local_rank in [None, 0]:
@@ -160,7 +160,7 @@ def setup_lstm_model(args, device, model_io_size):
     if bool(args.load_model_lstm):
         print('Load pretrained LSTM model:')
         print(args.pre_model_lstm)
-        model.load_state_dict(torch.load(args.pre_model_lstm))
+        model.load_state_dict(torch.load(args.pre_model_lstm, map_location=device))
     return model
 
 def blend(sz, sigma=0.5, mu=0.0):  