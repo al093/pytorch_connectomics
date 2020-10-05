9b22a55da3a04fe18fa93c780f0d8ce8b1acf2ed
Alok Verma <alok@alok.c.tensorflow-training-176320.internal>
2020-10-05 14:42:47 -0500

    read checkpoint files with generic model name

--- torch_connectomics/utils/net/misc.py
+++ torch_connectomics/utils/net/misc.py
@@ -115,6 +115,8 @@ def setup_model(args, device, model_io_size, exact=True, size_match=True, non_li
             checkpoint = torch.load(args.pre_model, map_location=device)
             if checkpoint.get(model.__class__.__name__ + '_state_dict', None):
                 model.load_state_dict(checkpoint[model.__class__.__name__ + '_state_dict'])
+            elif checkpoint.get('model_state_dict', None):
+                model.load_state_dict(checkpoint['model_state_dict'])
             else:
                 model.load_state_dict(checkpoint)
         else:
